2abb0cf2fda0d07480204ae37d7087e7b3b9be87
Pasha2580 <90571009+Pasha2580@users.noreply.github.com>
2021-10-21 13:10:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,1 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
 
-using namespace std;
-
-int main() {
-    int a = 5;
-    cout << a*2 << endl;
-    
-    return 0;
-}
