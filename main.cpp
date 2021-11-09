b2aee30283bf43c871b5de95ea45a1e99ab6096b
Pasha2580 <90571009+Pasha2580@users.noreply.github.com>
2021-11-10 00:58:44 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+//  считываем первую строку и преобразоуем ее в вектор
+//    string s;
+//    ifstream at ("F:\\homework1\\tr.txt");
+//    at.is_open();
+//    getline (at, s);
+//    int x_1;
+//    x_1 = stoi(s);
+//    string z = to_string(x_1);
+//    s.erase(s.find(z), z.length);
+//    int x_2;
+//    x_2 = stoi(s);
+//    at.close();
+
+    float arg_r = 0;
+    float arg_l = 0;
+    int r_1 = 0;
+    int r_2 = 0;
+    int l_1 = 0;
+    int l_2 = 0;
+//  Считывание файла и преобразование каждой компоненты в чсило
+    string str;
+    ifstream in ("F:\\homework1\\tr.txt");
+    in.is_open();
+    getline(in, str);
+    int x_1 = stoi(str);
+    string z = to_string(x_1);
+    str.erase(str.find(z), z.length());
+
+    if(in.is_open()){
+        while (getline (in,str)){
+//          координата х точки файла
+            int a_1;
+            a_1 = stoi(str);
+            string b = to_string(a_1);
+            str.erase(str.find(b), b.length());
+            int x_2 = stoi(str);
+//          координата y точки из файла
+            int a_2;
+            a_2 = stoi(str);
+            float ugol = acos((x_1 * a_1 + x_2 * a_2) / (sqrt(pow(x_1, 2) + pow(x_2, 2)) * sqrt(pow(a_1, 2) + pow(a_2, 2))));
+            if ((x_2 != 0 && a_1 >= x_1 * a_2 / x_2) || (x_2 == 0 && x_1 >= 0 && a_2 <= 0) || (x_2 == 0 && x_1 < 0 && a_2 >= 0)) {
+                if (ugol >= arg_r) {
+                    arg_r = ugol;
+                    r_1 = a_1;
+                    r_2 = a_2;
+                }
+            }
+            else {
+                if (ugol >= arg_l) {
+                    arg_l = ugol;
+                    l_1 = a_1;
+                    l_2 = a_2;
+                }
+            }
+        }
+    }
+    in.close();
+    cout << "Leftmost: " << l_1 << " " << l_2 << endl;
+    cout << "Rightmost: " << r_1 << " " << r_2 << endl;
+
+
+    return 0;
+}
